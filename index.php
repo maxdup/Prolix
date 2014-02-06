15f82ad89307d870cfc5f2a92a7fe4c8ba043e7f
maxdup <mdupuis@hotmail.ca>
2014-02-05 19:13:54 -0500

    Implemented Isotope js on theme index

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php get_header()?>
-
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/jquery-1.7.2.min.js" type="text/javascript"></script>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/jquery.isotope.min.js" type="text/javascript"></script>
 <div class="contentwrapper">
-	<div class="content">
+	<div id="isotope" class="content">
 		<?php while ( have_posts() ) : the_post();
-
-			/*
-			 * Include the post format-specific template for the content. If you want to
-			 * use this in a child theme, then include a file called called content-___.php
-			 * (where ___ is the post format) and that will be used instead.
-			 */
 			get_template_part( 'entry-index', get_post_format() );
-
 		endwhile;?>
 	</div>
-</div>
 
+	<script type="text/javascript">
+		$(window).load(function() {
+		  var $container = $('#isotope');
+
+		  $container.isotope({
+			itemSelector: '.item'
+		  });
+		  $('#iso').isotope( 'shuffle' );
+		  $container.isotope( 'option', { layoutMode: 'masonry' } );
+		  $('#iso').isotope( 'reLayout' );
+		});
+	</script>
+</div>
 <?php get_footer()?>
