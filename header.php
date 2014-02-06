d121bce35a0471aa7e5fa93b92f43f052eedfc1a
maxdup <mdupuis@hotmail.ca>
2014-02-06 08:33:03 -0500

    Implemented Less css

--- header.php
+++ header.php
@@ -4,8 +4,16 @@
 	<meta charset="utf-8" />
 	<title><?php bloginfo('title')?></title>
 	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
+
+	<!--<link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">-->
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/prolix.less" />
+	<script src="<?php bloginfo('stylesheet_directory'); ?>/js/less-1.6.2.min.js" type="text/javascript"></script>	
+
+	<?php  ?>
+
 	<?php wp_head()?>
+	
+	
 </head>
 
 <div id="container">
