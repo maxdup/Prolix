813536f9aa79ee83640faea02001917961544e7c
maxdup <mdupuis@hotmail.ca>
2014-03-01 09:22:41 -0500

    removed hardcoded elements for lightboxes

--- content-image.php
+++ content-image.php
@@ -1,12 +1,10 @@
 <div id="post-<?php the_ID(); ?>" class="item image <?php echo get_post_meta(get_the_ID(), 'dimension', True);$category = get_the_category(); echo ' ', str_replace(' ', '', $category[0]->cat_name); ?> isotope-item">
 	<?php if ( has_post_thumbnail() ) :
 		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'your_thumb_handle' );
-		if( empty( $post->post_content) ) : ?>
-			<img src="<?php echo $thumbnail['0']; ?>" />
-		<?php 
+		if( empty( $post->post_content) ) :
+			echo '<img src="', $thumbnail['0'],'" />';
 		else:
-			echo get_the_content() ?><img src="<?php echo $thumbnail['0']; ?>" /></a>
-			
-		<?php endif; ?>
-	<?php endif; ?>
+			echo '<a class="lightbox" href="', get_the_content(), '"><img src="', $thumbnail['0'], '"/></a>';
+		endif;
+	endif; ?>
 </div><!-- #post-## -->
