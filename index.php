64fb582a261fda4f02e6a14b1ebc27cb2e9ab64b
maxdup <mdupuis@hotmail.ca>
2014-03-03 08:16:29 -0500

    javascript refactoring

--- index.php
+++ index.php
@@ -21,37 +21,5 @@
 		</div>
 		<script src="<?php bloginfo('stylesheet_directory'); ?>/js/prolix.js" type="text/javascript"></script>
 	</div>
-	<script>	
-		(function() {
-    			function scrollHorizontally(e) {
-        			e = window.event || e;
-        			var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-        			document.getElementById('contentwrapper').scrollLeft -= (delta*50);
-        			e.preventDefault();
-    			}
-    			if (document.getElementById('contentwrapper').addEventListener) {
-        			// IE9, Chrome, Safari, Opera
-        			document.getElementById('contentwrapper').addEventListener("mousewheel", scrollHorizontally, false);
-        			// Firefox
-        			document.getElementById('contentwrapper').addEventListener("DOMMouseScroll", scrollHorizontally, false);
-    			} else {
-        			// IE 6/7/8
-        			document.getElementById('contentwrapper').attachEvent("onmousewheel", scrollHorizontally);
-    			}
-		})();
-
-		$(document).ready(function() {
-
-			$('.lightbox').magnificPopup({
-				type: 'image',
-				closeOnContentClick: true,
-				mainClass: 'mfp-img-mobile',
-				image: {
-					verticalFit: true
-				}
-		
-			});
-		});
-	</script>
 </div>
 <?php get_footer()?>
