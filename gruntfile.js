659c0eca68aa07f99ffd2d0f55c2bb663ce30b5b
maxdup <mdupuis@hotmail.ca>
2014-03-13 18:56:00 -0400

    more grunt tasks

--- gruntfile.js
+++ gruntfile.js
@@ -14,19 +14,41 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      styles: {
+      	styles: {
             // Which files to watch (all .less files recursively in the less directory)
-        files: ["*.less"],
-        tasks: ['less'],
-        options: {
-          livereload: true,
-        }
-      }
-    }
+        	files: ["*.less"],
+        	tasks: ['less'],
+        	options: {
+        	  	livereload: true,
+        	}
+      	}
+    },
+	concat: {
+options: {
+      	stripBanners: true,
+      		banner: '/* all changes made here will be overwritten eventually*/'
+    	},
+		js: {	
+			src: ['js/jquery-1.7.2.min.js',
+			'carousel/transition.js',
+			'carousel/carousel.js',
+			'js/jquery.isotope.min.js',
+			'js/prolix.js',
+			'jquery.magnific-popup.min.js'],	
+			dest: 'concat.js',
+		},
+		css: {
+			src: ['style.css',
+			'carousel/carousel.css',
+			'carousel/magnific-popup.css'
+			],
+			dest: 'concat.css',		
+		},
+	},
   });
-
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.registerTask('default', ['watch']);
 };
--- header.php
+++ header.php
@@ -6,16 +6,16 @@
 	<?php wp_head()?>
 	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 		
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url')?>">
+	<!--<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url')?>">
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/lightbox/magnific-popup.css" />
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory')?>/prolix.css">
-	<link rel="stylesheet" type="text/javascript" href="<?php bloginfo('stylesheet_directory'); ?>/carousel/carousel.css" media="all" >
-
 	
-	<script src="<?php bloginfo('stylesheet_directory'); ?>/js/jquery-1.7.2.min.js" type="text/javascript"></script>
-	<script src="<?php bloginfo('stylesheet_directory'); ?>/carousel/carousel.js" type="text/javascript"></script>
-	<script src="<?php bloginfo('stylesheet_directory'); ?>/carousel/transition.js" type="text/javascript"></script>
-	<script src="<?php bloginfo('stylesheet_directory'); ?>/lightbox/jquery.magnific-popup.min.js" type="text/javascript"></script>
+	<link rel="stylesheet" type="text/javascript" href="<?php bloginfo('stylesheet_directory'); ?>/carousel/carousel.css" media="all" >
+	-->
+
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory');?>/concat.css">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory')?>/prolix.css">
+
+	<script src="<?php bloginfo('stylesheet_directory'); ?>/concat.js" type="text/javascript"></script>
 </head>
 
 <div id="container">
