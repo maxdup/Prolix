87241f1d4bafd06656c34a98ebb563886d6f2057
maxdup <mdupuis@hotmail.ca>
2014-03-29 11:56:23 -0400

    css refactoring

--- content-image.php
+++ content-image.php
@@ -1,4 +1,4 @@
-<div id="post-<?php the_ID(); ?>" class="item image <?php if (is_category()): echo 'big '; endif; echo get_post_meta(get_the_ID(), 'dimension', True);$category = get_the_category(); echo ' ', str_replace(' ', '', $category[0]->cat_name); ?> isotope-item">
+<div id="post-<?php the_ID(); ?>" class="item picture <?php if (is_category()): echo 'big '; endif; echo get_post_meta(get_the_ID(), 'dimension', True);$category = get_the_category(); echo ' ', str_replace(' ', '', $category[0]->cat_name); ?> isotope-item">
 	<?php if ( has_post_thumbnail() ) :
 		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'your_thumb_handle' );
 		$url = wp_get_attachment_url(get_post_thumbnail_id($post->ID, 'thumbnail'));
