eff61a567d80dec50e6160a1bf6938a108f10382
maxdup <mdupuis@hotmail.ca>
2015-04-13 15:53:18 -0400

    fixed comments on pages

--- single.php
+++ single.php
@@ -2,15 +2,16 @@
 
 <div id="contentvertical">
 	<div class="contentblogpost">
+        <div class="pagecontent">
 		<?php while (have_posts()): the_post()?>
 			<h2><?php the_title()?></h2>
 			<?php echo '<div class="post-info">Posted by ',get_the_author_link() , ' on ', get_the_date(), '</div>';?> 
 			<?php the_content();?>
 			
 		<?php endwhile;?>
-
+        </div>
 		<?php comments_template();?>
-	<div>
+	</div>
 </div>
 
 <?php get_footer()?>
